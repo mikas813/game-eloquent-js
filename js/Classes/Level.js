8a972ce830ab651dd4087e8c1deae724a4ad3791
Mykhailo Lasiichuk <mishalasiychyk@MacBook-Air-Misha.local>
2020-10-24 07:58:48 +0100

    created Level class and

---
+++ js/Classes/Level.js
@@ -0,0 +1,19 @@
+class Level {
+	constructor(plan) {
+		let rows = plan.trim().split('\n').map(1 => [...1]);
+		this.height = rows.length;
+		this.width = rows[0].length;
+		this.startActors = [];
+
+		this.rows = rows.map((row, y) => {
+			return row.map((ch, x) => {
+				let type = levelChars[ch];
+				if (typeOf == 'string') return type;
+				this.startActors.push(
+					type.create(new Vec(x, y), ch));
+				return 'empty';
+
+			});
+		});
+	}
+}
